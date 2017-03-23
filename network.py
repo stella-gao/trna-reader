708bcd042f4f6bcaf64e102688dbe38026e109ca
Trevor Thai Kim Nguyen <trevortknguyen@gmail.com>
2017-03-23 09:09:01 -0500

    Add parsing a bit

---
+++ network.py
@@ -0,0 +1,13 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+import numpy as np
+
+print('Build the single LSTM model...')
+model = Sequential()
+model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
--- retrieve_fasta.py
+++ retrieve_fasta.py
@@ -1,4 +1,30 @@
 import urllib.request
 
+
 x = urllib.request.urlopen('http://gtrnadb.ucsc.edu/genomes/eukaryota/Scere3/sacCer3-tRNAs.fa')
-print(x.read())
+content = x.read().decode("utf-8")
+# now we have a list of the lines in the file
+lines = content.split('\n')
+
+print(len(lines))
+
+sequences = []
+seq_index = -1
+for i in range(len(lines)):
+    if lines[i].startswith('>'):
+        seq_index += 1
+        sequences.append('>')
+    else:
+        sequences[seq_index] += lines[i]
+
+print(sequences)
+print(len(sequences))
+
+# how much we tell the rnn before it has to guess
+test_length = 25
+
+# map the chars to numbers to feed in
+chars = sorted(list(set(text)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+# we have a forward-backward map of the chars to indices
